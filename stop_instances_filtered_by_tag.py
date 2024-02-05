60163d037084c68d79f3712f2201870204281964
Michele Costa <152905633+michele2c@users.noreply.github.com>
2024-02-05 15:05:49 -0700

    Update

--- stop_instances_filtered_by_tag.py
+++ stop_instances_filtered_by_tag.py
@@ -3,22 +3,15 @@ import boto3
 # Create an EC2 client
 ec2 = boto3.client('ec2')
 
-#Get all running instances
+# Get all running instances and tagged as Environment:Dev
 response = ec2.describe_instances(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']},
             {'Name': 'tag:Environment', 'Values': ['Dev']},]
 )
 
 for reservation in response['Reservations']:
-    # iterate over values stored in response and store values of 'Reservations' key
-    # in reservation
     for instance in reservation['Instances']:
         instance_id = instance['InstanceId']
-         # iterate over values stored in reservation and store values of 'Instances' key
-         # in instance
-        try:
-            ec2.stop_instances(InstanceIds=[instance_id])
-            print(f'Stopping instance: {instance_id} on Environment:Dev tag')
-        except:
-            print(f'Error when stopping: {instance_id')
-            
+
+        ec2.stop_instances(InstanceIds=[instance_id])
+        print(f'Stopping instance: {instance_id} on Environment:Dev tag')
