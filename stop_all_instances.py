252e1dc9f3ea2e0e94e493c2bc4cafa0179d07e0
Michele Costa <152905633+michele2c@users.noreply.github.com>
2024-02-05 15:04:12 -0700

    Update

--- stop_all_instances.py
+++ stop_all_instances.py
@@ -3,20 +3,13 @@ import boto3
 # Create an EC2 client
 ec2 = boto3.client('ec2')
 
-#Get all running instances
+# Get all running instances
 response = ec2.describe_instances(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-for reservation in response['Reservations']:
-    # iterate over values stored in response and store values of 'Reservations' key
-    # in reservation
-    for instance in reservation['Instances']:
-        instance_id = instance['InstanceId']
-         # iterate over values stored in reservation and store values of 'Instances' key
-         # in instance
-        try:
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+# Stop instances
             ec2.stop_instances(InstanceIds=[instance_id])
             print(f'Stopping instance: {instance_id}')
-        except:
-            print(f'Error when stopping: {instance_id')
-            
